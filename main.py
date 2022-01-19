d1fd976c62f2dba69ac59aa9953ad242e1298294
youyoungnam <dbdudska113@naver.com>
2022-01-19 11:39:48 +0900

    change main.py

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "MLOps"}
+    return {
+        "name": "Andys",
+        "age": 27,
+        "height": "11111",
+        "weight": "111111",
+        "address": "somewhere in the world"
+    }
 
 
 @app.get("/items/{item_id}")
